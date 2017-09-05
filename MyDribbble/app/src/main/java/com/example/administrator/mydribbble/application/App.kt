9760a9068acd6fee5b32d9ecbc41ab8ca9d1eac5
gao4281013 <gao4281013@gmail.com>
2017-09-05 23:24:31 +0800

    add MyDribbble

---
+++ MyDribbble/app/src/main/java/com/example/administrator/mydribbble/application/App.kt
@@ -0,0 +1,48 @@
+package com.example.administrator.mydribbble.application
+
+import android.app.Application
+import android.os.Environment
+import com.example.administrator.mydribbble.MainActivity
+import com.example.administrator.mydribbble.R
+import com.example.administrator.mydribbble.tools.Constant
+import com.example.administrator.mydribbble.tools.delegates.NotNullSingleValueVar
+import com.facebook.drawee.backends.pipeline.Fresco
+import com.liulishuo.filedownloader.FileDownloader
+import com.tencent.bugly.Bugly
+import com.tencent.bugly.beta.Beta
+import org.jetbrains.annotations.NotNull
+
+/**
+ * Created by Administrator on 2017/9/5 0005.
+ */
+class App:Application() {
+    //将application单例化，可供全局调用context
+    companion object {
+        var instance:App by NotNullSingleValueVar.DelegatesExt.notNullSingleValue()
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        init()
+    }
+
+    private fun init() {
+        instance = this
+        FileDownloader.init(applicationContext)
+        Thread{
+            Fresco.initialize(this)
+        }.start()
+
+    }
+
+    fun initBugly(){
+        Beta.initDelay = 3000
+        Beta.largeIconId = R.mipmap.ic_launcher
+        Beta.canShowUpgradeActs.add(MainActivity::class.java)
+        Beta.smallIconId = R.drawable.ic_update_black_24dp
+        Beta.storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS)
+        Bugly.init(applicationContext,Constant.BUGLY_ID,true)
+    }
+
+
+}
---
+++ MyDribbble/app/src/main/java/com/example/administrator/mydribbble/tools/Constant.kt
@@ -0,0 +1,12 @@
+package com.example.administrator.mydribbble.tools
+
+/**
+ * Created by Administrator on 2017/9/5 0005.
+ */
+class Constant {
+
+    companion object {
+
+        val BUGLY_ID = "ab01adf2e2"
+    }
+}
