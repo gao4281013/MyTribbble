8936333b923e6756ada4d32dff7f155ffea29875
gao4281013 <gao4281013@gmail.com>
2017-10-22 22:23:17 +0800

    add file

--- MyDribbble/app/src/main/java/com/example/administrator/mydribbble/view/adapter/CommentAdapter.kt
+++ MyDribbble/app/src/main/java/com/example/administrator/mydribbble/view/adapter/CommentAdapter.kt
@@ -1,10 +1,16 @@
 package com.example.administrator.mydribbble.view.adapter
 
 import android.support.v7.widget.RecyclerView
+import android.text.Html
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.administrator.mydribbble.R
 import com.example.administrator.mydribbble.entity.Comment
 import com.example.administrator.mydribbble.entity.Shot
+import com.example.administrator.mydribbble.tools.ImageLoad
+import com.example.administrator.mydribbble.tools.Utils
+import kotlinx.android.synthetic.main.item_comment.view.*
 
 /**
  * Created by Administrator on 2017/10/13.
@@ -17,21 +23,39 @@ class CommentAdapter( val mShot: Shot,
     val commentHintClick:() -> Unit,
     val countClick:(Int) -> Unit,
     val tagClick:(Int) -> Unit):RecyclerView.Adapter<CommentAdapter.ViewHolder>() {
-   private val HEAD =1
-   private val COMMENT = 2
-   private var  mFirstHolder:ViewHolder? = null
+    private val HEAD = 1
+    private val COMMENT = 2
+    private var mFirstHolder: ViewHolder? = null
 
-  override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
+        if (position!=0){
 
-  }
+        }
+    }
 
-  override fun getItemCount(): Int = mComments.size
+    override fun getItemCount(): Int = mComments.size
 
-  override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
+        if (viewType == COMMENT){
+            return ViewHolder(LayoutInflater.from(parent?.context).inflate(R.layout.item_comment,parent,false))
+        }else{
+            return ViewHolder(LayoutInflater.from(parent?.context).inflate(R.layout.item_details_head,parent,false))
+
+        }
+
+
   }
 
 
   inner class ViewHolder(itemView:View):RecyclerView.ViewHolder(itemView) {
-
+       fun bindComment(comment: Comment){
+           with(comment){
+               ImageLoad.frescoLoadCircle(itemView.mCommentAvatarImg,user?.avatar_url.toString())
+               itemView.mCommentText.text = Html.fromHtml(body)
+               itemView.mNameText.text = user?.name
+               itemView.mCommentLikeCount.text = likes_count.toString()
+               itemView.mTimeText.text = Utils.formatDateUseCh(Utils.parseISO8601(created_at!!))
+           }
+       }
   }
 }
