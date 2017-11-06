1c8c3adefe8b7c190a691d7da3010e6aa0235d91
gao4281013 <gao4281013@gmail.com>
2017-11-06 22:35:54 +0800

    add code

--- MyDribbble/app/src/main/java/com/example/administrator/mydribbble/view/activity/UserActivity.kt
+++ MyDribbble/app/src/main/java/com/example/administrator/mydribbble/view/activity/UserActivity.kt
@@ -1,12 +1,19 @@
 package com.example.administrator.mydribbble.view.activity
 
 import android.os.Bundle
+import android.support.v7.widget.DefaultItemAnimator
+import android.support.v7.widget.GridLayoutManager
 import com.example.administrator.mydribbble.R
 import com.example.administrator.mydribbble.entity.Shot
 import com.example.administrator.mydribbble.entity.User
 import com.example.administrator.mydribbble.presenter.UserPresenter
+import com.example.administrator.mydribbble.tools.singleData
 import com.example.administrator.mydribbble.view.adapter.UserShotAdapter
 import com.example.administrator.mydribbble.view.api.IUserView
+import kotlinx.android.synthetic.main.activity_user.*
+import org.greenrobot.eventbus.EventBus
+import org.greenrobot.eventbus.Subscribe
+import org.greenrobot.eventbus.ThreadMode
 
 class UserActivity : BaseActivity(),IUserView{
     lateinit private var mShots:MutableList<Shot>
@@ -33,7 +40,33 @@ class UserActivity : BaseActivity(),IUserView{
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user)
+        init()
+        EventBus.getDefault().register(this)
     }
+
+    private fun init() {
+        mPresenter = UserPresenter(this)
+        val layoutManager = GridLayoutManager(this,3)
+        mRecycleView.layoutManager = layoutManager
+        mRecycleView.itemAnimator = DefaultItemAnimator()
+        toolbar.inflateMenu(R.menu.search_menu)
+        mShots = mutableListOf(Shot())
+    }
+
+    @Subscribe(threadMode =ThreadMode.MAIN,sticky = true)
+    fun obtionUser(user: User){
+        mUser = user
+        mWho = if (singleData.isLogin() && user.avatar_url == singleData.avatar) MYSELF else OTHERS
+        mountData(user)
+    }
+
+    private fun mountData(user: User) {
+
+    }
+
+    /**
+     * 
+     * */
 
     override fun getShotSuccess(shots: MutableList<Shot>?, isLoadMore: Boolean) {
 
